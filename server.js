1dc73d3a81e74440c1b3bdf1a2150940bf684b6b
Michael Brady <michael.brady.ie@gmail.com>
2020-04-07 16:05:21 +0100

    changed apiurl to authentication backend (which is in a separate repo)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('rootpath')();
 const express = require('express');
 const app = express();
+const httpServer = require('http');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('src/_helpers/jwt');
@@ -16,7 +17,13 @@ app.use('/users', require('./users/users.controller'));
 
 app.use(errorHandler);
 
+const requestListener = function(req, res) {
+    res.writeHead(200);
+    res.end("Hello World")
+}
+
 const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+// const server = httpServer.createServer(requestListener)
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
